4e3e3255af6a5d733d77a069e75099bc7cca3f8d
hyunji <hyunji_2005@naver.com>
2025-09-19 11:39:52 +0900

    week03 : classes02 collections01

--- app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -30,7 +30,28 @@ class MainActivity : ComponentActivity() {
         week02Variables()
         week02Functions()
         week03classes()
+        week03Collections()
     }
+}
+
+private fun week03Collections(){
+    println("== Kotlin Collections ==")
+
+    val fruits = listOf("apple", "banana", "orange")
+    val mutableFruits = mutableListOf("kiwi", "watermelon")
+
+    println("Fruits : $fruits")
+    mutableFruits.add("banana")
+    println("Mutable fruits : $mutableFruits")
+
+    val scores = mapOf("Kim" to 100, "Park" to 97, "Lee" to 99)
+    println("Scores : $scores")
+
+    for(fruit in fruits){
+        println("I like $fruit")
+    }
+
+    scores.forEach{(name, score) -> println("$name scored $score")}
 }
 
 private fun week03classes(){
@@ -48,6 +69,14 @@ private fun week03classes(){
     student.name = "Mirae"
     student.age = 21
     student.introduce()
+
+    data class Person(val name: String, val age: Int)
+
+    val person1 = Person("Kim", 23)
+    val person2 = Person("Kim", 23)
+
+    println("Person1 : $person1")
+    println("Equal? ${person1 == person2}")
 }
 
 private fun week02Functions(){
