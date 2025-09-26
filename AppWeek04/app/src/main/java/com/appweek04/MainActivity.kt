697b682814007d478fec7a20eeee3d50adb4c11e
hyunji <hyunji_2005@naver.com>
2025-09-26 14:17:48 +0900

    week04 : Greeting App v0.2

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,38 +1,19 @@
 package com.appweek04
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editTextName = findViewById<EditText>(R.id.editTextName)
-        val buttonGreet = findViewById<Button>(R.id.buttonGreet)
-        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+        val buttonColor = findViewById<Button>(R.id.buttonGreet)
 
-        buttonGreet.setOnClickListener {
-            val name = editTextName.text.toString().trim()
-
-            var greeting: String = ""
-            if(name.isNotEmpty()){
-                greeting = "안녕하세요, ${name}님!"
-            }else{
-                greeting = "이름을 입력해주세요"
-            }
-
-            textViewGreeting.text = "이름을 입력해주세요"
-            textViewGreeting.visibility = View.VISIBLE
-            Log.d("KotlinWeek04App", "이름을 입력해주세요")
+        buttonColor.setOnClickListener {
+            startActivity(Intent(this, GreetingActivity::class.java))
         }
     }
 }
